2c1e21ea2e78d8336135bafe0eb6c3948e3fe6de
Julio Loch <julioloch@hotmail.com>
2022-06-25 05:17:30 -0300

    Variable counter and fizzbuzz exercises

---
+++ 04-variable-counter.js
@@ -0,0 +1,12 @@
+/* variableCounter
+When the user inputs a number
+Initialize a counter variable and set its value to zero
+While counter is smaller than user inputted number increment the counter by one
+Print the value of the counter variable */
+
+
+var number = 5;
+
+for(let counter=0; counter<number; counter++){
+    console.log(counter);
+}
